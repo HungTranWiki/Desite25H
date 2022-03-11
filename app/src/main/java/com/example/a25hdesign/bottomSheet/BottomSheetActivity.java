efc912b9e725a037cc248ee76fbb34f2ad8aa996
= <=>
2022-03-12 01:32:14 +0700

    commit

---
+++ app/src/main/java/com/example/a25hdesign/bottomSheet/BottomSheetActivity.java
@@ -0,0 +1,16 @@
+package com.example.a25hdesign.bottomSheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a25hdesign.R;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
